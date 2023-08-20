00e0322930c552fed578122b7a44c3443ef680ed
Robson VIlaça Lima <robsonvilaca.lima@gmail.com>
2023-08-20 11:35:51 -0300

    feat: Criando projeto Java

---
+++ 1.Backend/senac/senac/src/main/java/com/lojavirtual/senac/SenacApplication.java
@@ -0,0 +1,13 @@
+package com.lojavirtual.senac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenacApplication.class, args);
+	}
+
+}
---
+++ 1.Backend/senac/senac/src/main/java/com/lojavirtual/senac/model/Usuario.java
@@ -0,0 +1,9 @@
+package com.lojavirtual.senac.model;
+
+public class Usuario {
+    public int Id;
+    public String Email;
+    public String UserName;
+    public String Senha;
+    
+}
