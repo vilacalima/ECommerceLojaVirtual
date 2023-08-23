25e5a95b7a3c43f21652f68e2805dabf6ff8328c
isamidori <isabela_rodrigues97@hotmail.com>
2023-08-23 18:02:39 -0300

    form

--- 2.Frontend/originaly/src/App.js
+++ 2.Frontend/originaly/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import InputMask from 'react-input-mask';
 import './App.css';
 
 function App() {
@@ -11,58 +12,100 @@ function App() {
       </header> */}
       <main className="app-main">
         <div className="form-container">
-          <h2>Formulário</h2>
+          <h2>Cadastro de usuário</h2>
+
           <form>
             <div className="form-row">
               <div className="form-column">
                 <label htmlFor="name">Nome:</label>
                 <input type="text" id="name" name="name" />
               </div>
+
               <div className="form-column">
                 <label htmlFor="email">Email:</label>
+                <InputMask
+                  type="email"
+                  id="email"
+                  name="email"
+                  required
+                  pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
+                />
+              </div>
+            </div>
+            <div className="form-row">
+              <div className="form-column">
+                <label htmlFor="cpf">CPF:</label>
+                <input type="text" id="name" name="name" />
+              </div>
+              <div className="form-column">
+                <label htmlFor="Tele">Telefone:</label>
                 <input type="email" id="email" name="email" />
+
               </div>
-            </div>
-            <div className="form-row">
-              <label htmlFor="cpf">CPF:</label>
-              <input type="text" id="cpf" name="cpf" />
-            </div>
-            <div className="form-row">
-              <label htmlFor="phone">Telefone:</label>
-              <input type="text" id="phone" name="phone" />
-            </div>
-            <div className="form-row">
-              <div className="form-column">
-                <label htmlFor="uf">UF:</label>
-                <input type="text" id="uf" name="uf" />
+              <div className="form-row">
+                <div className="form-column">
+                  <label htmlFor="uf">UF:</label>
+                  <select id="uf" name="uf">
+                    <option value="AC">Acre</option>
+                    <option value="AL">Alagoas</option>
+                    <option value="AM">Amazonas</option>
+                    <option value="AP">Amapá</option>
+                    <option value="BA">Bahia</option>
+                    <option value="CE">Ceará</option>
+                    <option value="DF">Distrito Federal	</option>
+                    <option value="ES">Espírito Santo	</option>
+                    <option value="GO">Goiás</option>
+                    <option value="MA">Maranhão</option>
+                    <option value="MG">Minas Gerais</option>
+                    <option value="MS">Mato Grosso do Sul	</option>
+                    <option value="MT">Mato Grosso	</option>
+                    <option value="PA">Pará</option>
+                    <option value="PE">Pernambuco</option>
+                    <option value="PI">Piauí</option>
+                    <option value="PB">Paraíba</option>
+                    <option value="PR">Paraná</option>
+                    <option value="RJ">Rio de Janeiro	</option>
+                    <option value="RN">Rio Grande do Norte	</option>
+                    <option value="RO">Rondônia</option>
+                    <option value="RR">Roraima</option>
+                    <option value="RS">Rio Grande do Sul	</option>
+                    <option value="SC">Santa Catarina	</option>
+                    <option value="SE">Sergipe</option>
+                    <option value="SP">São Paulo	</option>
+                    <option value="TO">Tocantins</option>
+                  </select>
+                </div>
+                <div className="form-column">
+                  <label htmlFor="city">Cidade:</label>
+                  <input type="text" id="city" name="city" />
+                </div>
               </div>
-              <div className="form-column">
-                <label htmlFor="city">Cidade:</label>
-                <input type="text" id="city" name="city" />
-              </div>
-              <div className="form-column">
+              <div className="form-row">
                 <label htmlFor="cep">CEP:</label>
                 <input type="text" id="cep" name="cep" />
               </div>
+
+
+              <div className="form-row">
+                <div className="form-column">
+                  <label htmlFor="endereco">Endereço:</label>
+                  <input type="text" id="endereco" name="endereco" />
+                </div>
+                <div className="form-column">
+                  <label htmlFor="numero">Número e complemento:</label>
+                  <input type="text" id="numero" name="numero" />
+                </div>
+              </div>
+
+              <button type="submit">Cadastrar</button>
             </div>
-            <div className="form-row">
-              <label htmlFor="address">Endereço:</label>
-              <input type="text" id="address" name="address" />
-            </div>
-            <div className="form-row">
-              <label htmlFor="number">Número:</label>
-              <input type="text" id="number" name="number" />
-            </div>
-            <button type="submit">Cadastrar</button>
           </form>
         </div>
       </main>
-      
+
       <footer className="app-footer">
         <p>&copy; 2023 Minha Empresa</p>
       </footer>
     </div>
   );
-}
-
-export default App;
+} export default App;
