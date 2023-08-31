bc45620a45ee40f787f96faa612a6f878eab240c
isamidori <isabela_rodrigues97@hotmail.com>
2023-08-31 19:32:00 -0300

    inclusao tela listar sem ser admin

--- 2.Frontend/originaly/src/listarUsuariosAdmin.js
+++ 2.Frontend/originaly/src/listarUsuariosAdmin.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import './listarUsuariosAdmin.css'; 
 
-function ListaUsuarios() {
+function ListaUsuariosAdmin() {
   const [searchTerm, setSearchTerm] = useState('');
 
   const handleSearchChange = (event) => {
@@ -21,41 +22,53 @@ function ListaUsuarios() {
         onChange={handleSearchChange}
       />
 
-
+      <div className='adicionar'>
+      <Link to="/cadastrarUsuario" className="botao-adicionar"> Adicionar Usuário</Link>
+      </div>
       <table>
         <thead>
           <tr>
-
+            <th>Alterar</th>
             <th>Nome</th>
             <th>E-mail</th>
             <th>Status</th>
             <th>Grupo</th>
-
+            <th>Habilitar</th>
           </tr>
         </thead>
 
         <tbody id="userTable">
           <tr>
-
+            <td className="text-center">
+              <a className='btn btn-info btn-xs' href="#">
+                
+      <Link to="/alterarUsuario" className="botao-alterar"> Alterar</Link>
+              </a>
+            </td>
             <td>João</td>
             <td>joao@example.com</td>
             <td>Ativo</td>
             <td>--</td>
-
+            <td><input type="checkbox" checked /></td>
           </tr>
           <tr>
-
+          <td className="text-center">
+              <a className='btn btn-info btn-xs' href="#">
+                
+      <Link to="/alterarUsuario" className="botao-alterar"> Alterar</Link>
+              </a>
+            </td>
             <td>Maria</td>
             <td>maria@example.com</td>
             <td>Inativo</td>
             <td>--</td>
-
+            <td><input type="checkbox" /></td>
           </tr>
-
+          {/* Mais linhas de usuário podem ser adicionadas aqui */}
         </tbody>
       </table>
     </div>
   );
 }
 
-export default ListaUsuarios;
+export default ListaUsuariosAdmin;
