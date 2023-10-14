36a3677b25aab6880793d10199648a720ab0238e
isamidori <isabela_rodrigues97@hotmail.com>
2023-10-14 18:46:39 -0300

    Update cadastrarCliente.js
    validar cep e complementar endereço. Validar quantidade minima de letras no nome

--- 2.Frontend/originaly/src/components/cliente/cadastrarCliente.js
+++ 2.Frontend/originaly/src/components/cliente/cadastrarCliente.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class CadastroCliente extends Component {
   constructor() {
@@ -10,18 +11,18 @@ class CadastroCliente extends Component {
       telefone: '',
       enderecos: [
         {
+          cep: '',
           rua: '',
           numero: '',
           complemento: '',
           bairro: '',
           cidade: '',
-          estado: '',
-          cep: '',
+          uf: '',
           padrao: true,
         },
       ],
       dataNascimento: '',
-      sexo: 'Masculino', // Pode definir um valor padrão
+      sexo: 'Masculino',
       senha: '',
     };
   }
@@ -31,15 +32,21 @@ class CadastroCliente extends Component {
     this.setState({ [name]: value });
   };
 
+  handleEnderecoChange = (index, name, value) => {
+    const enderecos = [...this.state.enderecos];
+    enderecos[index][name] = value;
+    this.setState({ enderecos });
+  };
+
   adicionarEndereco = () => {
     const novoEndereco = {
+      cep: '',
       rua: '',
       numero: '',
       complemento: '',
       bairro: '',
       cidade: '',
-      estado: '',
-      cep: '',
+      uf: '',
       padrao: false,
     };
 
@@ -62,8 +69,66 @@ class CadastroCliente extends Component {
     this.setState({ enderecos });
   };
 
-  handleSubmit = (event) => {
+  
+  validarCEP = async (cep, index) => {
+    try {
+      const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+      const data = response.data;
+
+      if (data.erro) {
+        alert('CEP inválido');
+      } else {
+        const enderecos = [...this.state.enderecos];
+        enderecos[index] = {
+          ...enderecos[index],
+          cep: data.cep,
+          rua: data.logradouro,
+          complemento: data.complemento,
+          bairro: data.bairro,
+          cidade: data.localidade,
+          uf: data.uf,
+        };
+        this.setState({ enderecos });
+      }
+    } catch (error) {
+      console.error('Erro ao validar CEP:', error);
+    }
+  };
+
+  handleSubmit = async (event) => {
     event.preventDefault();
+
+    // Validar o nome do cliente
+    const nomeValido = /^([A-Za-z]{3,}\s[A-Za-z]{3,})$/.test(this.state.nome);
+
+    if (!nomeValido) {
+      alert('O nome deve conter duas palavras com pelo menos 3 letras em cada.');
+      return;
+    }
+
+    // Validar o CPF
+    if (!this.validarCPF(this.state.cpf)) {
+      alert('CPF inválido');
+      return;
+    }
+
+    // Validar se o CPF já existe
+    const cpfJaExiste = this.state.cpf !== '' && this.state.enderecos.some((cliente) => cliente.cpf === this.state.cpf);
+    if (cpfJaExiste) {
+      alert('CPF já cadastrado');
+      return;
+    }
+
+    // Validar o endereço de faturamento
+    const enderecoFaturamento = this.state.enderecos.find((endereco) => endereco.padrao);
+    if (!enderecoFaturamento) {
+      alert('Endereço de faturamento é obrigatório');
+      return;
+    }
+
+    // Validar outros campos, como CPF único
+    // ...
+
     // Aqui você pode adicionar a lógica para enviar os dados do formulário para o servidor
     // Por exemplo, usando uma solicitação HTTP para uma API de backend
     console.log(this.state); // Isso irá mostrar os dados no console, para fins de demonstração
@@ -71,9 +136,10 @@ class CadastroCliente extends Component {
 
   render() {
     return (
-      <div>
-        <h1>Cadastro de Cliente</h1>
+      <div className="form-container">
+        <h1>Cadastrar Cliente</h1>
         <form onSubmit={this.handleSubmit}>
+          {/* Campos de dados pessoais */}
           <div>
             <label>Nome:</label>
             <input type="text" name="nome" value={this.state.nome} onChange={this.handleChange} required />
@@ -90,22 +156,105 @@ class CadastroCliente extends Component {
             <label>Telefone:</label>
             <input type="text" name="telefone" value={this.state.telefone} onChange={this.handleChange} required />
           </div>
+          <div>
+            <label>Data de nascimento:</label>
+            <input type="date" name="dataNascimento" value={this.state.dataNascimento} onChange={this.handleChange} required />
+          </div>
+          <div>
+            <label>Sexo:</label>
+            <input type="radio" name="sexo" value="Masculino" onChange={this.handleChange} required /> Masculino
+            <input type="radio" name="sexo" value="Feminino" onChange={this.handleChange} required /> Feminino
+            <input type="radio" name="sexo" value="Outro" onChange={this.handleChange} required /> Outro
+          </div>
+          <div>
+            <label>Senha:</label>
+            <input type="password" name="senha" value={this.state.senha} onChange={this.handleChange} required />
+          </div>
+
+          {/* Campos de endereço */}
           {this.state.enderecos.map((endereco, index) => (
             <div key={index}>
               <h2>Endereço {index + 1}</h2>
-              {/* Campos de endereço aqui */}
-              <button type="button" onClick={() => this.removerEndereco(index)}>
-                Remover
-              </button>
+              <div>
+                <label>CEP:</label>
+                <input
+                  type="text"
+                  name={`cep[${index}]`}
+                  value={endereco.cep}
+                  onChange={(e) => this.handleEnderecoChange(index, 'cep', e.target.value)}
+                  onBlur={(e) => this.validarCEP(e.target.value, index)} // Adicione o onBlur para validar o CEP quando sair do campo
+                  required
+                />
+              </div>
+              <div>
+                <label>Logradouro:</label>
+                <input
+                  type="text"
+                  name={`rua[${index}]`}
+                  value={endereco.rua}
+                  onChange={(e) => this.handleEnderecoChange(index, 'rua', e.target.value)}
+                  required
+                />
+              </div>
+              <div>
+                <label>Número:</label>
+                <input
+                  type="text"
+                  name={`numero[${index}]`}
+                  value={endereco.numero}
+                  onChange={(e) => this.handleEnderecoChange(index, 'numero', e.target.value)}
+                  required
+                />
+              </div>
+              <div>
+                <label>Complemento:</label>
+                <input
+                  type="text"
+                  name={`complemento[${index}]`}
+                  value={endereco.complemento}
+                  onChange={(e) => this.handleEnderecoChange(index, 'complemento', e.target.value)}
+                />
+              </div>
+              <div>
+                <label>Bairro:</label>
+                <input
+                  type="text"
+                  name={`bairro[${index}]`}
+                  value={endereco.bairro}
+                  onChange={(e) => this.handleEnderecoChange(index, 'bairro', e.target.value)}
+                  required
+                />
+              </div>
+              <div>
+                <label>Cidade:</label>
+                <input
+                  type="text"
+                  name={`cidade[${index}]`}
+                  value={endereco.cidade}
+                  onChange={(e) => this.handleEnderecoChange(index, 'cidade', e.target.value)}
+                  required
+                />
+              </div>
+              <div>
+                <label>UF:</label>
+                <input
+                  type="text"
+                  name={`uf[${index}]`}
+                  value={endereco.uf}
+                  onChange={(e) => this.handleEnderecoChange(index, 'uf', e.target.value)}
+                  required
+                />
+              </div>
+
               <button type="button" onClick={() => this.definirComoPadrao(index)}>
                 Definir como Padrão
               </button>
             </div>
           ))}
           <button type="button" onClick={this.adicionarEndereco}>
-            Adicionar Endereçoo
+            Adicionar Endereço
           </button>
-          {/* Outros campos de cadastro, como data de nascimento, sexo e senha */}
+
           <button type="submit">Cadastrar</button>
         </form>
       </div>
