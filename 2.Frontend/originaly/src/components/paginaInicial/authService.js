3ee706371b6eb97e71549763abb420a07d3f8698
Robson Vilaça <robsonvilaca.lima@gmail.com>
2023-11-01 21:51:52 -0300

    feat: Criando um cabeçalho e ajustando o header

--- 2.Frontend/originaly/src/components/paginaInicial/authService.js
+++
@@ -1,6 +0,0 @@
-export const logout = () => {
-  // Realize qualquer ação necessária para deslogar o usuário aqui.
-
-  // Redirecione o usuário para a página de login ou a página inicial.
-  window.location.href = '/login';
-};
