0385bf6c3b90d3cf462b99241c68ada2c1558aae
Robson Vilaça <robsonvilaca.lima@gmail.com>
2023-11-06 22:34:46 -0300

    feat: Ajustando página de pedido

---
+++ 2.Frontend/originaly/src/components/compra/itensPedido.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import './pagamento.css';
+import CarrinhoService from '../../service/carrinhoService';
+
+function ItensPedido() {
+  const [carrinho, setCarrinho] = useState([]);
+
+  useEffect(() => {
+    
+    try {
+        const loggedInUser = localStorage.getItem("usuario");
+        let carrinho = '';
+        if (loggedInUser != null){
+            const usuarioParse = JSON.parse(loggedInUser);
+            carrinho = usuarioParse.email;
+        } else {
+            carrinho = 'Usuario_nao_logado';
+        }
+        
+        loadCarrinho(carrinho);
+    } catch (error) {
+    console.log(error);
+    }
+  }, []);
+
+  const loadCarrinho = async (email) => {
+    const dto = await CarrinhoService.getCarrinhoTemporario(email)
+    setCarrinho(dto);
+  };
+  
+  return (
+    <div className='borda'>
+      <div>
+        <label>
+          Itens do pedido:
+        </label>
+      </div>
+
+      <div>
+        {carrinho.map((item, index) => (
+          <div key={index}>
+            <p>Produto({index + 1}): {item.nomeProduto} Quantidade: {item.quantidade}  
+             Preço Unitário: R$ {item.precoUnitario} Preço Total: R$ {item.precoTotal}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+  
+}
+
+export default ItensPedido;
